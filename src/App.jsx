4462c187adc8950c1de911a53753ea2791859e8b
Francisco <frankiiize.gmail.com>
2021-12-28 17:48:52 -0300

    configuracion vercel

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1>Hola react! ahora estoy en vercel !</h1>
+  )
+}
+
+export { App };
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,37 @@ module.exports = {
   output: {
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+          options: {
+            presets : [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			}
+	
+		]
+	},
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html'
     })
-  ]
+  ],
+ 
 }
